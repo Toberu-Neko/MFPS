b5cd0ec0fb1b49b46985ac049f83071bca2fd9b1
u10914226 <u10914226@ms.ttu.edu.tw>
2022-09-13 11:50:55 +0800

    no message

--- Assets/Scripts/Motion.cs
+++ Assets/Scripts/Motion.cs
@@ -6,33 +6,65 @@ namespace Com.Neko.SelfLearning
 {
     public class Motion : MonoBehaviour
     {
-        public float orgSpeed, sprintSpeed;
+        public float orgSpeed, sprintSpeed, jumpForce;
         private Rigidbody rig;
+        public LayerMask ground;
+        public Transform groundDetector;
+
+        public Camera normalCam;
+        private float defultFOV;
+        public float sprintFOVModifier = 1.45f;
+
         void Start()
         {
+            defultFOV = normalCam.fieldOfView;
             Camera.main.enabled = false;
             rig = GetComponent<Rigidbody>();
         }
 
         void FixedUpdate()
         {
+            //Input
             float t_hmove = Input.GetAxisRaw("Horizontal");//水平A+1, D-1
             float t_vmove = Input.GetAxisRaw("Vertical");//垂直W+1, S=1
-            float t_adjustedSpeed = orgSpeed;
-
             bool sprint = Input.GetKey(KeyCode.RightShift) || Input.GetKey(KeyCode.LeftShift);
-            bool isSprinting = sprint;
+            bool jump = Input.GetKeyDown(KeyCode.Space);
 
+            //States
+            bool isGrounded = Physics.Raycast(groundDetector.position, Vector3.down, 0.1f, ground);//Raycast(偵測目標位置, 偵測方向, 偵測離主角距離，小於為真, layerMask)
+            bool isJumping = jump && isGrounded;
+            bool isSprinting = sprint && t_vmove > 0 && !isJumping && isGrounded;
+
+            //Jumping
+            if (isJumping)
+            {
+                rig.AddForce(Vector3.up * jumpForce);
+            }
+
+            //Movement
             Vector3 t_direction = new Vector3(t_hmove, 0, t_vmove);
             t_direction.Normalize();
+            float t_adjustedSpeed = orgSpeed;
 
-            if (isSprinting && t_vmove < 0) 
+            if (isSprinting) 
             {
                 t_adjustedSpeed *= sprintSpeed;
             }
+            Vector3 t_targetVelocity = transform.TransformDirection(t_direction) * t_adjustedSpeed * Time.deltaTime;
+            t_targetVelocity.y = rig.velocity.y;
+            rig.velocity = t_targetVelocity;
 
+            //FOV
+            if (isSprinting)
+            {
+                //Lerp(a,b,c)=a經過c秒變成b
+                normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, defultFOV * sprintFOVModifier, Time.deltaTime * 8f);
+            }
+            else
+            {
+                normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, defultFOV, Time.deltaTime * 8f);
+            }
 
-            rig.velocity = transform.TransformDirection(t_direction) * t_adjustedSpeed * Time.deltaTime;
         }
     }
 }
