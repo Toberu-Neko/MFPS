15fb13731d9101088019c85ddd4294f8df2e7fc4
謝清華 <u10914226@ms.ttu.edu.tw>
2022-12-02 07:22:53 +0800

    血條調整，尚未製作Lerp

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -7,39 +7,56 @@ using Photon.Pun;
 public class PlayerStatus : MonoBehaviourPunCallbacks
 {
     private Slider healthBar;
-    public int maxHealth;
-    public int currentHealth;
+    [SerializeField]private int maxHealth;
+    private int currentHealth;
     private SpawnPlayer spawnPlayer;
+    float orgHealth;
     void Start()
     {
-        spawnPlayer = GameObject.Find("GameManager").GetComponent<SpawnPlayer>();
-        currentHealth = maxHealth;
+        if (photonView.IsMine)
+        {
+            spawnPlayer = GameObject.Find("GameManager").GetComponent<SpawnPlayer>();
+            currentHealth = maxHealth;
 
 
-        healthBar = UIManager.get.UI.transform.Find("HUD/HealthBar/Slider").gameObject.GetComponent<Slider>();
-        healthBar.maxValue = maxHealth;
-        healthBar.value = currentHealth;
-
-
+            healthBar = UIManager.get.UI.transform.Find("HUD/HealthBar/Slider").gameObject.GetComponent<Slider>();
+            healthBar.maxValue = maxHealth;
+            healthBar.value = currentHealth;
+            orgHealth = currentHealth;
+        }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.U))
+        //Debug.Log(Mathf.Lerp(100, 50, 8f * Time.deltaTime));
+        if (photonView.IsMine)
         {
-            TakeDamage(maxHealth/2);
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                TakeDamage(maxHealth / 5);
+            }
+           // RefreshHealthBar(orgHealth, currentHealth);
+            if(transform.position.y < -100f)
+            {
+                TakeDamage(maxHealth);
+            }
         }
     }
-
+    void RefreshHealthBar(float _current, float _target)
+    {
+        healthBar.value = Mathf.Lerp(_current, _target, 8f * Time.deltaTime);
+    }
     public void TakeDamage(int _damage)
     {
         if (photonView.IsMine)
         {
+            orgHealth = currentHealth;
             currentHealth -= _damage;
             healthBar.value = currentHealth;
+            //RefreshHealthBar(orgHealth, currentHealth);
             //Debug.Log("Player" + gameObject.GetPhotonView() + " health = " + currentHealth);
 
-            if(currentHealth <= 0)
+            if (currentHealth <= 0)
             {
                 currentHealth = 0;
                 spawnPlayer.Spawn();
